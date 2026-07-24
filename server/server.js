88f0c2bce3950c670b47c09e3c1b9323bed1c021
Fabio Paixao <fabio.paixao.globo@gmail.com>
2026-07-24 18:30:20 -0300

    Fix Express 5 wildcard routing using app.use

--- server/server.js
+++ server/server.js
@@ -116,8 +116,8 @@ app.post('/api/bookings/cancel', async (req, res) => {
     }
 });
 
-// Catch-all route to// Serve index.html for all other routes (SPA fallback)
-app.get('/(.*)', (req, res) => {
+// Serve index.html for all other routes (SPA fallback)
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
